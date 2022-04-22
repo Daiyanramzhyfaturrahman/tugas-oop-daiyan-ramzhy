a27062974c2acac305fec0ecfdfb4f326a8313cd
Daiyanramzhyfaturrahman <103202004+Daiyanramzhyfaturrahman@users.noreply.github.com>
2022-04-23 01:27:30 +0700

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.looping;
+
+
+public class Looping {
+
+    public static void main(String[] args) {
+        var motor = 55;
+        
+        for(;motor <= 73;){
+            System.out.println("perulangan " + motor);
+            
+            motor++;
+        }
+         int mobil = 55;
+         
+         while(mobil <= 73 ){
+        System.out.println("perulangan" + mobil);
+        
+        mobil++;
+    }
+    }
+}
+
